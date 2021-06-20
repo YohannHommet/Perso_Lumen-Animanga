79072b68ace3b02d87ae65e7eda477c0a7c4b1b7
YohannHommet <yohann.hommet@outlook.fr>
2021-06-20 20:18:23 +0200

    Adding footer and Procfile

--- resources/views/_partials/_footer.blade.php
+++ resources/views/_partials/_footer.blade.php
@@ -1,3 +1,13 @@
-<footer class="footer">
 
+<footer class="footer border-top p-5">
+    <div class="row">
+        <div class="col text-center">
+
+            <a href="mailto:yohann.hommet@outlook.fr" class="text-muted text-decoration-none fs-5">Yohann Hommet</a>
+            <p>Copyright &copy; <?= date("Y") ?></p>
+
+            <small class="mt-5 d-block">API From <a href="https://kitsu.docs.apiary.io/#">Kitsu</a></small>
+
+        </div>
+    </div>
 </footer>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,8 +7,8 @@
     <div class="container-fluid p-0 m-0 g-0 error__container">
         <section class="error__section" style="background-image: url('{{ url('assets/img/error-404.png') }}')">
 
-            <h2 class="error__section_title">Naaaaniiiiiiiii !!</h2>
-            <a href="{{ @route('home') }}" class="error__section_link text-center">Come back home bro</a>
+            <h2 class="error__section_title fs-1">Nani ?!</h2>
+            <a href="{{ @route('home') }}" class="error__section_link text-center fs-5">Come back home bro</a>
 
         </section>
     </div>
